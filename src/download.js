ef188788580fce49caad6b934fc359e0d04522d8
Sophie <73658711+charcoalspore4@users.noreply.github.com>
2023-06-17 18:24:08 +0000

    Update download.js for aug 2022

--- src/download.js
+++ src/download.js
@@ -1,4 +1,4 @@
-import data from "./2022_JULY.json";
+import data from "./2022_AUGUST.json";
 function headers({ csv }) {
   return (csv += "Name,Address,Latitude,Longitude,StartTimeStamp,EndTimeStamp");
 }
@@ -42,5 +42,5 @@ export function download() {
   let csv = "";
   csv = headers({ csv });
   csv = body({ csv });
-  generate({ csv, name: "2022_JULY" });
+  generate({ csv, name: "2022_AUGUST" });
 }
