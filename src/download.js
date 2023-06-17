4a253b38d95a3ce2793b34f7ec99f3e7894bfa97
Sophie <73658711+charcoalspore4@users.noreply.github.com>
2023-06-17 18:17:19 +0000

    Update 2 files to download feb 2023

--- src/download.js
+++ src/download.js
@@ -1,4 +1,4 @@
-import data from "./2023_JANUARY.json";
+import data from "./2023_FEBRUARY.json";
 function headers({ csv }) {
   return (csv += "Name,Address,Latitude,Longitude,StartTimeStamp,EndTimeStamp");
 }
@@ -42,5 +42,5 @@ export function download() {
   let csv = "";
   csv = headers({ csv });
   csv = body({ csv });
-  generate({ csv, name: "January" });
+  generate({ csv, name: "2023_FEBRUARY" });
 }
